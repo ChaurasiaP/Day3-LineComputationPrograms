d3a5c854519569f67f363192940b3ae578563cc1
Pranshu Chaurasia <66831697+ChaurasiaP@users.noreply.github.com>
2023-01-19 17:45:05 +0530

    Update LineComputationPrograms.java

--- LineComputationPrograms.java
+++ LineComputationPrograms.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 public class LineComputationPrograms {
     public static void main(String[] args) {
 
-        System.out.println("Line Computation: UC2: Check for equality of lines");
+        System.out.println("Line Computation: UC3: Compare the lines");
         // VARIABLES TO CALCULATE LINE 1
         int x1, y1;     //  co-ordinates of first point
         int x2, y2;     //  co-ordinates of second point
