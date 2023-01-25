990848c8385f44f1a4ebd8ab0c72c9803926714e
Pranshu Chaurasia <66831697+ChaurasiaP@users.noreply.github.com>
2023-01-25 10:35:13 +0530

    added UC4 of Day8 line comparison program

--- LineComputationPrograms.java
+++ LineComputationPrograms.java
@@ -1,22 +1,49 @@
+package lineComparison;
+
 import java.util.Scanner;
 
-public class LineComputationPrograms {
-    static double LengthOfLine1;
-    static double LengthOfLine2;
+public class LineComparison {
+    //DAY 8 UC4 LINE COMPARISON
 
+    // VARIABLES TO CALCULATE LINE 1
+    public static int x1, y1,x2,y2;
+
+    // VARIABLES TO CALCULATE LINE 2
+    public static int x3, y3,x4,y4;
+    
+    public static double line1, line2;  // length of line 1  and line 2
+    
+     // METHOD FOR CALCULATIONS FOR LINES
+    public void getLength() {
+        line1 = Math.sqrt((Math.pow((x2 - x1), 2)) + Math.pow((y2 - y1), 2));
+        System.out.println("Length of Line 1: "+line1);
+
+        line2 = Math.sqrt((Math.pow((x4 - x3), 2)) + Math.pow((y4 - y3), 2));
+        System.out.println("Length of Line 2: "+line2);
+
+    }
+
+    public void checkLine(double line1, double line2) {
+
+        Double L1 = line1;
+        Double L2 = line2;
+
+        if ((L1.compareTo(L2) < 0)) {
+            System.out.println("Line 1 is shorter than Line 2");
+        } else if ((L1.compareTo(L2) > 0)) {
+            System.out.println("Line 1 is greater than Line 2");
+        } else {
+            System.out.println("Both Lines are Equal");
+        }
+    }
+
+    // MAIN METHOD STARTS
     public static void main(String[] args) {
-
 
         System.out.println("Line Computation: Using Method");
 
-        // VARIABLES TO CALCULATE LINE 1
-        int x1, y1;     //  co-ordinates of first point
-        int x2, y2;     //  co-ordinates of second point
-
-        // VARIABLES TO CALCULATE LINE 2
-        int x3, y3;     // co-ordinates of third point
-        int x4, y4;     // co-ordinates of fourth point
-
+        LineComparison getLen = new LineComparison();
+        LineComparison checkLen = new LineComparison();
 
         //TAKING INPUTS FOR COMPUTING LINE 1
         System.out.println("Co-ordinates for line 1");
@@ -29,13 +56,6 @@ public class LineComputationPrograms {
         x2 = sc.nextInt();
         y2 = sc.nextInt();
 
-        // CALCULATIONS FOR LINE1
-
-        double ValOfx = Math.pow((x2 - x1), 2);
-        double ValOfy = Math.pow((y2 - y1), 2);
-        LengthOfLine1 = Math.sqrt((ValOfx + ValOfy));
-        // END OF CALCULATIONS FOR LINE 1.
-
         // TAKING INPUTS FOR LINE 2
         System.out.println("Co-ordinates for line 2");
         System.out.print("Enter co-ordinates x3 and y3: ");
@@ -46,27 +66,11 @@ public class LineComputationPrograms {
         x4 = sc.nextInt();
         y4 = sc.nextInt();
 
-        // CALCULATIONS FOR LINE 2
-        double ValOfX = Math.pow((x4 - x3), 2);
-        double ValOfY = Math.pow((y4 - y3), 2);
-        LengthOfLine2 = Math.sqrt((ValOfX + ValOfY));
-        // END OF CALCULATIONS FOR LINE 2
+        getLen.getLength();
+        System.out.println("Comparing Lines: ");
 
-        checkLine(LengthOfLine1, LengthOfLine2);
-    }
-
-    public static void checkLine(double length1, double length2) {
-
-        Double L1 = LengthOfLine1;
-        Double L2 = LengthOfLine2;
-
-        if ((L1.compareTo(L2) < 0)) {
-            System.out.println("Line 1 is shorter than Line 2");
-        } else if ((L1.compareTo(L2) > 0)) {
-            System.out.println("Line 1 is greater than Line 2");
-        } else {
-            System.out.println("Both Lines are Equal");
-        }
+        checkLen.checkLine(line1, line2);
     }
 }
+
 // Program ends here
